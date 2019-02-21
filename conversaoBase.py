b3b4acff72bd535ffe8a73ea28d0e9b95dfed80f
LuizHenriqueBO <luiz_hickbarros@hotmail.com>
2019-02-20 22:20:38 -0300

    Método de conversão de bases

---
+++ conversaoBase.py
@@ -0,0 +1,17 @@
+
+def dectoBin(valor):
+    return bin(valor)
+
+def dectoOctal(valor):
+    return oct(valor)
+
+def dectoHex(valor):
+    return hex(valor)
+
+
+
+print(dectoBin(25.3))
+print(dectoOctal(25.3))
+print(dectoHex(25.3))
+
+
